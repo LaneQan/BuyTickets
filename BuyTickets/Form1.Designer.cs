187afe70f9965ed3ff47311597fb1a46baf12d16
LaneQan <laneqan1@gmail.com>
2016-10-16 11:55:13 +0300

    Update

--- BuyTickets/Form1.Designer.cs
+++ BuyTickets/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelLogin = new MaterialSkin.Controls.MaterialLabel();
+            this.labelPassword = new MaterialSkin.Controls.MaterialLabel();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.Enter = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialSingleLineTextField2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.Register = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Depth = 0;
+            this.labelLogin.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelLogin.Location = new System.Drawing.Point(41, 96);
+            this.labelLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(82, 29);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Логин";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Depth = 0;
+            this.labelPassword.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelPassword.Location = new System.Drawing.Point(41, 146);
+            this.labelPassword.MouseState = MaterialSkin.MouseState.HOVER;
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(97, 29);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.materialSingleLineTextField1.Hint = "";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(146, 96);
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(165, 23);
+            this.materialSingleLineTextField1.TabIndex = 2;
+            this.materialSingleLineTextField1.UseSystemPasswordChar = false;
+            // 
+            // Enter
+            // 
+            this.Enter.AutoSize = true;
+            this.Enter.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Enter.Depth = 0;
+            this.Enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Enter.Location = new System.Drawing.Point(146, 193);
+            this.Enter.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Enter.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Enter.Name = "Enter";
+            this.Enter.Primary = false;
+            this.Enter.Size = new System.Drawing.Size(58, 36);
+            this.Enter.TabIndex = 3;
+            this.Enter.Text = "Войти";
+            this.Enter.UseVisualStyleBackColor = true;
+            // 
+            // materialSingleLineTextField2
+            // 
+            this.materialSingleLineTextField2.Depth = 0;
+            this.materialSingleLineTextField2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.materialSingleLineTextField2.Hint = "";
+            this.materialSingleLineTextField2.Location = new System.Drawing.Point(146, 146);
+            this.materialSingleLineTextField2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField2.Name = "materialSingleLineTextField2";
+            this.materialSingleLineTextField2.PasswordChar = '*';
+            this.materialSingleLineTextField2.SelectedText = "";
+            this.materialSingleLineTextField2.SelectionLength = 0;
+            this.materialSingleLineTextField2.SelectionStart = 0;
+            this.materialSingleLineTextField2.Size = new System.Drawing.Size(165, 23);
+            this.materialSingleLineTextField2.TabIndex = 4;
+            this.materialSingleLineTextField2.UseSystemPasswordChar = false;
+            // 
+            // Register
+            // 
+            this.Register.AutoSize = true;
+            this.Register.Depth = 0;
+            this.Register.Location = new System.Drawing.Point(154, 255);
+            this.Register.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Register.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Register.Name = "Register";
+            this.Register.Primary = false;
+            this.Register.Size = new System.Drawing.Size(184, 36);
+            this.Register.TabIndex = 5;
+            this.Register.Text = "Не зарегистрированы?";
+            this.Register.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(350, 300);
+            this.Controls.Add(this.Register);
+            this.Controls.Add(this.materialSingleLineTextField2);
+            this.Controls.Add(this.Enter);
+            this.Controls.Add(this.materialSingleLineTextField1);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialLabel labelLogin;
+        private MaterialSkin.Controls.MaterialLabel labelPassword;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialFlatButton Enter;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField2;
+        private MaterialSkin.Controls.MaterialFlatButton Register;
     }
 }
 
