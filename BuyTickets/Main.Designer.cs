f3e6797266a5ac339185dfcb64bea08dcd7f1bdb
LaneQan <laneqan1@gmail.com>
2016-10-27 07:23:27 +0300

    12

--- BuyTickets/Main.Designer.cs
+++ BuyTickets/Main.Designer.cs
@@ -28,31 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
             this.SuspendLayout();
             // 
-            // vScrollBar1
+            // listView1
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(783, 66);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(17, 440);
-            this.vScrollBar1.TabIndex = 0;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Location = new System.Drawing.Point(12, 103);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(976, 685);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(12, 81);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(204, 19);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "Сеансы на данный момент";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 600);
-            this.Controls.Add(this.vScrollBar1);
+            this.ClientSize = new System.Drawing.Size(1000, 800);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.listView1);
             this.Name = "Main";
             this.Text = "BuyTickets";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.ListView listView1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
     }
 }
