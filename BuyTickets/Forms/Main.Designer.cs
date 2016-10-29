5b586c86e7a6e1b6cb365108a1b0064f43b6d3be
LaneQan <laneqan1@gmail.com>
2016-10-29 20:01:25 +0300

    register query sql

---
+++ BuyTickets/Forms/Main.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BuyTickets
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Info;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(0, 110);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(830, 578);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Mad Max.jpg");
+            this.imageList1.Images.SetKeyName(1, "X-men.jpg");
+            this.imageList1.Images.SetKeyName(2, "Воздушная тюрьма.jpg");
+            this.imageList1.Images.SetKeyName(3, "Выживший.jpg");
+            this.imageList1.Images.SetKeyName(4, "Колония.jpg");
+            this.imageList1.Images.SetKeyName(5, "Марсианин.jpg");
+            this.imageList1.Images.SetKeyName(6, "Мстители.jpg");
+            this.imageList1.Images.SetKeyName(7, "Ультраамериканцы.jpg");
+            this.imageList1.Images.SetKeyName(8, "Училка.jpg");
+            this.imageList1.Images.SetKeyName(9, "Человек-муравей.jpg");
+            this.imageList1.Images.SetKeyName(10, "Шерлок Холмс.jpg");
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(19, 77);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(204, 19);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "Сеансы на данный момент";
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(621, 71);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(170, 33);
+            this.materialRaisedButton1.TabIndex = 2;
+            this.materialRaisedButton1.Text = "Личный кабинет";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(500, 77);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(114, 19);
+            this.materialLabel2.TabIndex = 3;
+            this.materialLabel2.Text = "Баланс: 2,7 руб";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 688);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.listView1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Sizable = false;
+            this.Text = "BuyTickets";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private System.Windows.Forms.ImageList imageList1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+    }
+}
---
+++ BuyTickets/Forms/Register.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+using System.Data.SQLite;
+
+namespace BuyTickets
+{
+    public partial class Register : MaterialForm
+    {
+        public Register()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            var Login = materialSingleLineTextField2.Text;
+            var Mail = materialSingleLineTextField1.Text;
+            var Pass = materialSingleLineTextField3.Text;
+            var Name = materialSingleLineTextField4.Text;
+            var Surname = materialSingleLineTextField5.Text;
+            var Phone = materialSingleLineTextField6.Text;
+
+            // DATABASE
+
+            const string databaseName = @"X:\Learning\BuyTickets\BuyTickets\DB\BT.sqlite";
+            SQLiteConnection connection = new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+            connection.Open();
+            SQLiteCommand cmd = new SQLiteCommand("INSERT INTO 'Users' (Login, Password, Name, Surname, Phone, Mail, isAdmin)"
+                + "VALUES (@LoginParam, @PasswordParam, @NameParam, @SurnameParam, @PhoneParam, @MailParam, @isAdmin)", connection);
+            cmd.Parameters.AddWithValue("@LoginParam", Login);
+            cmd.Parameters.AddWithValue("@PasswordParam", Pass);
+            cmd.Parameters.AddWithValue("@NameParam", Name);
+            cmd.Parameters.AddWithValue("@SurnameParam", Surname);
+            cmd.Parameters.AddWithValue("@PhoneParam", Phone);
+            cmd.Parameters.AddWithValue("@MailParam", Mail);
+            cmd.Parameters.AddWithValue("@isAdmin", false);
+            cmd.ExecuteNonQuery();
+            connection.Close(); 
+        }
+    }
+}
